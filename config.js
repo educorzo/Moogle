3bee5e3da30ca0652c7e3ad417508848380ef09c
Eduardo Corzo <edukorzo@gmail.com>
2021-03-27 15:33:10 +0100

    Updating CD

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const authorizeSpotifyUri = 'https://accounts.spotify.com/authorize?response_type=token'
-const redirectUri = 'http://localhost:8080/' // This parameter must be registered in Spotify
-const clientId = 'babf2f19af8144b1a31371477968389e' // This value should be set when deploying
+
+const redirectUri = 'https://educorzo.github.io/Moogle/' // This parameter must be registered in Spotify
+const clientId = '0088361d36ae4c6c8e4efe3b5e6b89d7' // This value should be set when deploying
 
 export default {
   loggingSpotifyUri: authorizeSpotifyUri + '&redirect_uri=' + redirectUri + '&client_id=' + clientId,
